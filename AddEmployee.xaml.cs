c1e8019ca4152758c1888c29a2ff6dc6cbee7047
Humphryshikunzi <39647238+Humphryshikunzi@users.noreply.github.com>
2019-02-06 15:47:03 +0300

    Add files via upload
    Files for employee uwp app for adding new employees to my register

---
+++ AddEmployee.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Employees
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AddEmployee : Page
+    {
+        public AddEmployee()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var employee = new Employee()
+            {
+                Name = EmployeeNameTb.Text,
+                Department = EmployeeDeptTb.Text
+            };
+            var employeejson = JsonConvert.SerializeObject(employee);
+            var client=new HttpClient();
+            var httpcontent=new StringContent(employeejson);
+            httpcontent.Headers.ContentType=new MediaTypeHeaderValue("application/json");
+            await client.PostAsync("http://localhost:62463/api/employees", httpcontent);
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Employees
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            HttpClient http=new HttpClient();
+            var clientresponse = await http.GetStringAsync("http://localhost:62463/api/employees");
+            var employeesresponse = JsonConvert.DeserializeObject<List<Employee>>(clientresponse);
+            EmployeesListBox.ItemsSource = employeesresponse;
+
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(AddEmployee));
+        }
+
+        private void EmployeesListBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var employee = EmployeesListBox.SelectedItem as Employee;
+            Frame.Navigate(typeof(EditEmployee),employee);
+        }
+    }
+}
